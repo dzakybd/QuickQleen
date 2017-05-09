f9a78a7c0999be92082d3cf4dde59b724c7a1f63
dzakybd <dzakybd@gmail.com>
2017-05-09 13:45:45 +0700

    Laundry!

--- app/src/main/java/id/qq/quickqleen/History.java
+++ app/src/main/java/id/qq/quickqleen/History.java
@@ -2,31 +2,17 @@ package id.qq.quickqleen;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class History extends AppCompatActivity {
 
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
-    @BindView(R.id.image)
-    CircleImageView image;
-    @BindView(R.id.view1)
-    CardView view1;
-    @BindView(R.id.textView)
-    TextView textView;
-    @BindView(R.id.view2)
-    CardView view2;
-    @BindView(R.id.button2)
-    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
