c8fc79d3e654e4535388e6226b3ff2eff56a7ac3
dzakybd <dzakybd@gmail.com>
2017-05-09 10:17:22 +0700

    Laundry!

--- app/src/main/java/id/qq/quickqleen/History.java
+++ app/src/main/java/id/qq/quickqleen/History.java
@@ -1,13 +1,56 @@
 package id.qq.quickqleen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class History extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.image)
+    CircleImageView image;
+    @BindView(R.id.nama)
+    TextView nama;
+    @BindView(R.id.plat)
+    TextView plat;
+    @BindView(R.id.view)
+    CardView view;
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.textView2)
+    TextView textView2;
+    @BindView(R.id.button2)
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        ButterKnife.bind(this);
+        setTitle("Pesanan 1");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
